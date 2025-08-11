0bc2e0252a23ebfb4f21e403d5c16bf57750c0ca
IronOps-Max <verytorque@outlook.com>
2025-08-11 20:00:55 +0100

    push

--- scripts/install_dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-cd /home/ubuntu/flask-app
-# ensure pip3 is available
-if ! command -v pip3 >/dev/null 2>&1; then
-  echo "pip3 not found, please ensure Python3 & pip3 are installed"
-  exit 1
-fi
-pip3 install -r requirements.txt
--- scripts/start_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-cd /home/ubuntu/flask-app
-# kill previous process (if any)
-pkill -f "python3 app.py" || true
-# start app in background and redirect logs
-nohup python3 app.py > app.log 2>&1 &
